13f07f6152e771e2d7284efe3cb06aa6a20ede63
Ismaël Lachheb <ismael.lachheb@protonmail.com>
2019-07-31 19:34:36 +0200

    added screenshot of vat

--- docs/source/about.rst
+++
@@ -1,8 +0,0 @@
-About us
-========
-
-
-Core Development Team
----------------------
-
-Ismaël Lachheb (initialized the project in 2019): `Homepage <https://lachhebo.github.io/>`_
--- pyclustertend/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-name = "pyclustertend"
-
-from .hopkins import hopkins
-from .VAT import vat, ordered_dissimilarity_matrix
-
-# TODO: add version information here
-#from . import metrics
-#__all__ = ['metrics']
