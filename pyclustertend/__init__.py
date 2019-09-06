73fe687a5517ab134fa523d72dd2bf40e29297e8
Ismaël Lachheb <ismael.lachheb@protonmail.com>
2019-09-06 20:01:38 +0200

    feature assess tendency by metrics

--- pyclustertend/__init__.py
+++ pyclustertend/__init__.py
@@ -3,7 +3,7 @@ name = "pyclustertend"
 
 from .hopkins import hopkins
 from .VAT import vat, ordered_dissimilarity_matrix
-from .metric import assess_tendency_silhouette, assess_tendency_calinski_harabaz
+from .metric import assess_tendency_silhouette, assess_tendency_calinski_harabaz, assess_tendency_davies_bouldin, assess_tendency_by_metrics
 
 
 # TODO: add version information here
