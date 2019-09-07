b0dcd8bff9839a671a51c432f4d9184069d08403
Ismaël Lachheb <ismael.lachheb@protonmail.com>
2019-09-07 08:02:24 +0200

    fix : documentation missing dependency 6

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,7 @@ release = '1.3.3'
 # -- General configuration ---------------------------------------------------
 
 source_suffix = '.rst'
-#master_doc = 'source/index'
+master_doc = 'index'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
