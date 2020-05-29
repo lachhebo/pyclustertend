f6b6b75d3721721e218db789619f8151298255a0
Ismaël <ismail.lachheb@octo.com>
2020-05-29 13:32:54 +0200

    version upgrade

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyclustertend",
-    version="1.4.0",
+    version="1.4.9",
     author="Ismaël Lachheb",
     author_email="ismael.lachheb@protonmail.com",
     description="A package to assess cluster tendency",
