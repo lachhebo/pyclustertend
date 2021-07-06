fix: issue with ivat matrix computation (thanks to Tim Havens)

pyclustertend/visual_assessment_of_tendency.py
@@ -1,6 +1,6 @@
+import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.metrics import pairwise_distances
-import matplotlib.pyplot as plt
 
 
 def vat(data, return_odm=False, figure_size=(10, 10)):
@@ -167,13 +167,14 @@ def compute_ivat_ordered_dissimilarity_matrix(X):
     re_ordered_matrix = np.zeros((ordered_matrix.shape[0], ordered_matrix.shape[0]))
 
     for r in range(1, ordered_matrix.shape[0]):
-        # Step 1 : find j for which D[r,j] is minimum and j in [1:r-1]
+        # Step 1 : find j for which D[r,j] is minimum and j ipn [1:r-1]
 
         j = np.argmin(ordered_matrix[r, 0:r])
 
         # Step 2 :
 
         re_ordered_matrix[r, j] = ordered_matrix[r, j]
+        re_ordered_matrix[j, r] = ordered_matrix[r, j]
 
         # Step 3 : pour c : 1,r-1 avec c !=j
         c_tab = np.array(range(0, r))

tests/test_visual_assessment_of_tendency.py
@@ -1,7 +1,9 @@
-import numpy as np
 from pathlib import Path
-from sklearn import datasets
 from unittest.mock import patch
+
+import numpy as np
+from sklearn import datasets
+from sklearn.preprocessing import scale
 
 from pyclustertend import vat, ivat, compute_ordered_dissimilarity_matrix, compute_ivat_ordered_dissimilarity_matrix
 
@@ -16,19 +18,6 @@ def test_compute_ordered_dissimilarity_matrix():
 
     # when
     ordered_matrix = compute_ordered_dissimilarity_matrix(iris_dataset)
-
-    # then
-    np.testing.assert_allclose(ordered_matrix, expected_ordered_matrix, atol=0.1)
-
-
-def test_compute_ivat_ordered_dissimilarity_matrix():
-    # given
-    iris = datasets.load_iris()
-    iris_dataset = iris.data
-    expected_ordered_matrix = np.load(TEST_DIR / 'data/iris_ivat.npy')
-
-    # when
-    ordered_matrix = compute_ivat_ordered_dissimilarity_matrix(iris_dataset)
 
     # then
     np.testing.assert_allclose(ordered_matrix, expected_ordered_matrix, atol=0.1)
@@ -88,3 +77,14 @@ def test_vat_does_not_return_the_matrix_by_default(mock_compute_vat):
 
     # then
     assert output_result is None
+
+
+def test_compute_ivat_ordered_dissimilarity_matrix__return_symetric_matrix():
+    # given
+    x = scale(datasets.load_iris().data)
+
+    # when
+    result = compute_ivat_ordered_dissimilarity_matrix(x)
+
+    # then
+    np.testing.assert_allclose(result, result.T, atol=0.01)